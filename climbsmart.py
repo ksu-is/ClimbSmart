f64e57fdedda51b5277f6ef2da4af90d8fdb6477
jgilley56 <jgillen2@students.kennesaw.edu>
2026-04-22 20:45:42 -0400

    Session
    Add climbing session input

--- climbsmart.py
+++ climbsmart.py
@@ -1,4 +1,7 @@
 print("Welcome to ClimbSmart")
 
 name = input("Enter your name: ")
-print("Hello", name)
+session_type = input("Type of climbing (bouldering/sport): ")
+duration = input("How long did you climb (minutes): ")
+
+print("Session logged for", name)
