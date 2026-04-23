9350758322eb37cd0ab3f86f405c9c77fc829f58
jgilley56 <jgillen2@students.kennesaw.edu>
2026-04-22 20:47:37 -0400

    Update climbsmart.py
    Add file saving for session data

--- climbsmart.py
+++ climbsmart.py
@@ -5,11 +5,16 @@ session_type = input("Type of climbing (bouldering/sport): ")
 duration = input("How long did you climb (minutes): ")
 
 pain = input("Did you feel pain? (yes/no): ")
+area = ""
 
 if pain.lower() == "yes":
     area = input("Where is the pain (fingers/wrist/elbow): ")
     print("Pain recorded in", area)
 else:
     print("No pain recorded")
+
+file = open("sessions.txt", "a")
+file.write(name + "," + session_type + "," + duration + "," + pain + "," + area + "\n")
+file.close()
 
 print("Session logged for", name)
