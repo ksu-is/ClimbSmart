8a5c56c9fa0077d499681ebc5821cf4d6b2f3bf8
jgilley56 <jgillen2@students.kennesaw.edu>
2026-05-01 10:27:20 -0400

    Update climbsmart.py

--- climbsmart.py
+++ climbsmart.py
@@ -3,7 +3,7 @@ print("Welcome to ClimbSmart")
 name = input("Enter your name: ")
 session_type = input("Type of climbing (bouldering/sport): ")
 duration = input("How long did you climb (minutes): ")
-
+intensity = input("Rate the session intensity from 1-10: ")
 pain = input("Did you feel pain? (yes/no): ")
 area = ""
 
@@ -14,13 +14,14 @@ else:
     print("No pain recorded")
 
 file = open("sessions.txt", "a")
-file.write(name + "," + session_type + "," + duration + "," + pain + "," + area + "\n")
+file.write(name + "," + session_type + "," + duration + "," + intensity + "," + pain + "," + area + "\n")
 file.close()
 
 print("\n--- SESSION SUMMARY ---")
 print("Name:", name)
 print("Type:", session_type)
 print("Duration:", duration)
+print("Intensity:", intensity)
 print("Pain:", pain)
 if area != "":
     print("Pain Area:", area)
